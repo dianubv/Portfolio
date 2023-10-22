42650a90e3e082b760547d5091ebdc7a875fa7b4
dianubv <diane.bouffardvercelli@gmail.com>
2023-10-22 12:13:47 +0200

    build pb 2

--- .nuxt/dist/api/ipx.js
+++
@@ -1,3 +0,0 @@
-import { createIPX, createIPXMiddleware } from "ipx";
-const ipx = createIPX({"dir":"/home/diane/Documents/MDT/Portfolio/static","domains":[],"sharp":{},"alias":{}});
-export default createIPXMiddleware(ipx);
--- .nuxt/dist/server/pages/works/workPage.js
+++
@@ -1,3 +0,0 @@
-exports.ids=[18],exports.modules={104:function(p,e,t){"use strict";t.r(e);var s=function(){var r=this,l=r.$createElement,u=r._self._c||l;return u("div",[r._ssrNode("<h1>"+r._ssrEscape(r._s(r.project))+"</h1>")])},a=[],o={computed:{project(){return this.$route.params.project}}},n=o,c=t(2),v=Object(c.a)(n,s,a,!1,null,null,"23469fa8"),i=e.default=v.exports}};
-
-//# sourceMappingURL=workPage.js.map
