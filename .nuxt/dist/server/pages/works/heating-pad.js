4d587d5b78f5d6d1275ffd12a84a327a3fa8648f
dianubv <diane.bouffardvercelli@gmail.com>
2023-10-23 10:28:53 +0200

    maybe last commit for doployment :')

---
+++ .nuxt/dist/server/pages/works/heating-pad.js
@@ -0,0 +1,3 @@
+exports.ids=[17],exports.modules={103:function(l,r,t){"use strict";t.r(r);var a=function(){var e=this,v=e.$createElement,i=e._self._c||v;return i("div",[e._ssrNode("<h1>"+e._ssrEscape(e._s(e.project))+"</h1>")])},s=[],n={computed:{project(){return this.$route.params.project}}},o=n,c=t(2),p=Object(c.a)(o,a,s,!1,null,null,"f8e69408"),u=r.default=p.exports}};
+
+//# sourceMappingURL=heating-pad.js.map
